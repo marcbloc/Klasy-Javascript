08de6f0cf1afe5bb3641dc77a475a671be9836f3
marblo <marcinblochowiak@wp.pl>
2017-05-15 20:32:26 +0200

    3

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ Telephone.prototype.printInfo = function() {
 	console.log("Marka telefonu to " + this.name + ", kolor to " + this.color + "cena to " + this.price + ".");
 }
 var iPhones6s = new Telephone("Apple", 2250, "srebrny");
-var SamsungGalaxyS6 = new Telephone("Samsung Galaxy 6s", 2000, "niebieski");
-var OnePlusOne = new Telephone("One Plus One", 1500, "żólty");
+var samsungGalaxyS6 = new Telephone("Samsung Galaxy 6s", 2000, "niebieski");
+var onePlusOne = new Telephone("One Plus One", 1500, "żólty");
 
 iPhones6s.printInfo();
 SamsungGalaxyS6.printInfo();
